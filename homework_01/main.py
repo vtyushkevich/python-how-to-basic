c4b231752b42e5ecf951a67611061fe6d20ba993
vadim <vadim.tyushkevich@yandex.ru>
2022-06-23 11:34:36 +0500

    Added solution for filter_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -23,7 +23,21 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_prime(numbers):
+    results = []
+    for num1 in numbers:
+        if type(num1) is int and num1 != 0:
+            k = True
+            for i in range(2, int(num1 // 2 + 1)):
+                if (num1 % i) == 0:
+                    k = False
+                    break
+            if k and num1 != 1:
+                results.append(num1)
+    return results
+
+
+def filter_numbers(numbers, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -34,3 +48,15 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+
+    if filter_type == "even":
+        return list(filter(lambda x: x % 2 == 0, numbers))
+    if filter_type == "odd":
+        return list(filter(lambda x: x % 2 != 0, numbers))
+    if filter_type == "prime":
+        return is_prime(numbers)
+
+
+for num_type in [ODD, EVEN, PRIME]:
+    print(filter_numbers(list(range(0, 20)), num_type))
+
