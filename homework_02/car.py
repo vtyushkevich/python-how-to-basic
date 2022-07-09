fd381453bb87634d17d272dacc27f9c37e6c0721
vadim <vadim.tyushkevich@yandex.ru>
2022-07-09 19:20:58 +0500

    Added Car class to car.py

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,18 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from base import Vehicle
+from engine import Engine
+
+
+class Car(Vehicle):
+
+    def __init__(self, weight, started, fuel, fuel_consumption):
+        super().__init__(weight, started, fuel, fuel_consumption)
+        self.engine = None
+        self.volume = None
+        self.pistons = None
+
+    def set_engine(self, engine):
+        self.volume = engine.volume
+        self.pistons = engine.pistons
