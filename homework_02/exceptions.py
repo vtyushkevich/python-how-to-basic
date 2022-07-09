4f1a1ea9169a73dd8ae01487ab5e271df08ef3a8
vadim <vadim.tyushkevich@yandex.ru>
2022-07-09 17:19:31 +0500

    Added exceptions with pass to exceptions.py

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,21 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class Error(Exception):
+    pass
+
+
+class LowFuelError(Error):
+    pass
+
+
+class NotEnoughFuel(Error):
+    pass
+
+
+class CargoOverload(Error):
+    pass
+
+
