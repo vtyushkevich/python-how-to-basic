75c3d094ab8e3dc54134b441f54f38cd6ef15f23
vadim <vadim.tyushkevich@yandex.ru>
2022-07-09 18:42:17 +0500

    Added Vehicle class to base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,60 @@
 from abc import ABC
+from exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    weight = 0.0
+    started = True
+    fuel = 0.0
+    fuel_consumption = 0.0
+
+    def __init__(self, weight, started, fuel, fuel_consumption):
+        self.weight = weight
+        self.started = started
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        print("----> start")
+        try:
+            if not self.started:
+                if self.fuel > 0:
+                    self.started = True
+                else:
+                    raise LowFuelError
+            else:
+                print("____It's already started____")
+        except LowFuelError:
+            print("!!!___Low Fuel___!!!")
+
+    def move(self, distance=0):
+        print("----> move to", distance, "km")
+        try:
+            if self.fuel_consumption * distance / 100 <= self.fuel:
+                self.fuel -= (self.fuel_consumption * distance / 100)
+            else:
+                raise NotEnoughFuel
+        except NotEnoughFuel:
+            print("!!!___Not Enough Fuel___!!!")
+
+    def stop(self):
+        print("----> stop")
+        self.started = False
+
+
+# my_car = Vehicle(1000, False, 10.0, 5.0)
+# print("my_car.started =", my_car.started, ", my_car.fuel =", my_car.fuel, ", my_car.fuel_consumption =", my_car.fuel_consumption)
+# my_car.start()
+# print("my_car.started =", my_car.started, ", my_car.fuel =", my_car.fuel, ", my_car.fuel_consumption =", my_car.fuel_consumption)
+# my_car.start()
+# print("my_car.started =", my_car.started, ", my_car.fuel =", my_car.fuel, ", my_car.fuel_consumption =", my_car.fuel_consumption)
+# my_car.move(200)
+# print("my_car.started =", my_car.started, ", my_car.fuel =", my_car.fuel, ", my_car.fuel_consumption =", my_car.fuel_consumption)
+# my_car.stop()
+# print("my_car.started =", my_car.started, ", my_car.fuel =", my_car.fuel, ", my_car.fuel_consumption =", my_car.fuel_consumption)
+# my_car.start()
+# print("my_car.started =", my_car.started, ", my_car.fuel =", my_car.fuel, ", my_car.fuel_consumption =", my_car.fuel_consumption)
+
+
+
+
