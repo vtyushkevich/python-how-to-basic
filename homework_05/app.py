ed42d5d45534a70de48eaca2e28dc73ce25e8d4a
vadim <vadim.tyushkevich@yandex.ru>
2022-09-10 22:32:50 +0500

    001 homework 05 - copied files for the homework - start dev

---
+++ homework_05/app.py
@@ -0,0 +1,24 @@
+"""
+Домашнее задание №5
+Первое веб-приложение
+
+создайте базовое приложение на Flask
+создайте index view /
+добавьте страницу /about/, добавьте туда текст
+создайте базовый шаблон (используйте https://getbootstrap.com/docs/5.0/getting-started/introduction/#starter-template)
+в базовый шаблон подключите статику Bootstrap 5 и добавьте стили, примените их
+в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
+в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
+"""
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    # print_request()
+    return "<h1>Hello, World!</h1>"
+    # return render_template("index.html")
+    # return render_template("base.html")
